e03e1a77b3695719dcc2ca5f942b69a0675ea12f
Sebastian Källstedt <sk223dn@student.lnu.se>
2018-01-03 15:43:00 +0100

    Started working on connection for chat and sending messages

--- src/js/Chat.js
+++ src/js/Chat.js
@@ -14,6 +14,29 @@ class Chat {
   print () {
     let template = document.querySelector('#chat-template').content.cloneNode(true)
     this.element.querySelector('.application-content').appendChild(template)
+
+    this.element.querySelector('form').addEventListener('submit', this.submitMessage.bind(this))
+  }
+
+  async connectToServer () {
+    this.socket = await new window.WebSocket(this.server)
+  }
+  submitMessage (event) {
+    event.preventDefault()
+
+    let message = this.element.querySelector('.message-text').value
+
+    if (message > 1) {
+      let msg = {
+        type: 'message',
+        data: message,
+        username: this.username,
+        channel: this.channel,
+        key: this.key
+      }
+
+      this.socket.send(JSON.stringify(msg))
+    }
   }
 }
 
