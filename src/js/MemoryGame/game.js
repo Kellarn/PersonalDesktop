210e8c241c1d39846d4ba5e2f870ad46272e56b4
Sebastian Källstedt <sk223dn@student.lnu.se>
2018-01-05 15:11:08 +0100

    Created a shuffle function for the cards, seems to work

--- src/js/MemoryGame/game.js
+++ src/js/MemoryGame/game.js
@@ -14,24 +14,37 @@ class MemoryGame {
     this.imageNumberArray = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7]
     this.currentGameImgAmount = this.imageNumberArray.slice(0, (this.x * this.y))
     this.turns = 0
+
+    this.shuffleCards()
   }
 
   initialization () {
     let i = 0
 
     this.boardArray = []
-    for (i = 0; i < this.x; i++) {
+    for (i = 0; i < this.y; i++) {
       let array = []
       array.length = this.y
       this.boardArray.push(array)
     }
 
     for (i = 0; i < this.y; i++) {
-      for (let x = 0; x < this.x; x++) {
+      for (let x = 0; x < this.x - 1; x += 2) {
         this.boardArray[i][x] = new Card(i + x, this.currentGameImgAmount.pop())
-        this.boardArray[i][x + 1] = new Card(i + x, this.currentGameImgAmount.pop())
+        this.boardArray[i][x + 1] = new Card(i + (x + 1), this.currentGameImgAmount.pop())
       }
-      console.log(this.boardArray)
+    }
+    console.log(this.boardArray)
+  }
+
+  shuffleCards () {
+    let temp
+    let random
+    for (let i = 0; i < this.currentGameImgAmount.length; i++) {
+      temp = this.currentGameImgAmount[i]
+      random = Math.floor(Math.random() * this.currentGameImgAmount.length)
+      this.currentGameImgAmount[i] = this.currentGameImgAmount[random]
+      this.currentGameImgAmount[random] = temp
     }
   }
 }
