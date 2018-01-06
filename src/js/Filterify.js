2e9b0f676f08c11b807580f08d499534d48b9617
Sebastian Källstedt <sk223dn@student.lnu.se>
2018-01-06 20:37:41 +0100

    Update

--- src/js/Filterify.js
+++ src/js/Filterify.js
@@ -10,13 +10,7 @@ class Filterify {
       video: true
     }
   }
-
-<<<<<<< HEAD
-  initialization () {
-    console.log(this.element)
-=======
   async initialization () {
->>>>>>> 234ac1762fdb6a42eb87aa40d1fad729bdb8493b
     this.print()
     await this.startUp()
     this.handleSuccess()
@@ -25,25 +19,16 @@ class Filterify {
   print () {
     let template = document.querySelector('#filterify-template').content.cloneNode(true)
     this.element.querySelector('.application-content').appendChild(template)
-<<<<<<< HEAD
-=======
   }
 
   async startUp () {
->>>>>>> 234ac1762fdb6a42eb87aa40d1fad729bdb8493b
     this.video = this.element.querySelector('#video')
     this.canvas = this.element.querySelector('#canvas')
     this.photo = this.element.querySelector('#photo')
     this.startButton = this.element.querySelector('#startButton')
-  }
 
-<<<<<<< HEAD
-  startUp () {
-    navigator.mediaDevices.getUserMedia(this.constraints).then(this.handleSuccess())
-=======
     await navigator.mediaDevices.getUserMedia(this.constraints)
     return navigator.mediaDevices.getUserMedia(this.constraints)
->>>>>>> 234ac1762fdb6a42eb87aa40d1fad729bdb8493b
   }
   handleSuccess (stream) {
     let videoTracks
